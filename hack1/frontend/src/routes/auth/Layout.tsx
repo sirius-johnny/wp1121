22b18f4cdb8e8cfdbfac2aeba70e179a50298492
sirius-johnny <jj955104@gmail.com>
2023-10-12 12:12:45 +0800

    hack1_third_111

--- hack1/frontend/src/routes/auth/Layout.tsx
+++ hack1/frontend/src/routes/auth/Layout.tsx
@@ -77,7 +77,7 @@ const AuthLayout = () => {
                 className="last-of-type:border-r-0"
                 data-testid={`tab-${tab.path}`}
               >
-                <NavLink to={tab.title} />
+                <NavLink to={`/${tab.path}`}>{tab.title}</NavLink>
               </TabsTrigger>
               /* End of TODO 1.3 */
             ))}
